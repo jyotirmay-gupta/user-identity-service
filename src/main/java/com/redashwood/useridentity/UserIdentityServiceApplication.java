86c49617caa840f4f5e52bc566e1bfb9c30dd3d3
jyotirmay-gupta <jyotirmay.gupta@outlook.com>
2025-07-25 08:40:20 +0530

    Named main thread for logging.

--- src/main/java/com/redashwood/useridentity/UserIdentityServiceApplication.java
+++ src/main/java/com/redashwood/useridentity/UserIdentityServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserIdentityServiceApplication {
 
     public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        t.setName("User-Identity-Service");
         SpringApplication.run(UserIdentityServiceApplication.class, args);
     }
 
